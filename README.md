7809a4cf256972d67986685b7e9acabe9b3af1cc
whatbeato <96067139+whatbeato@users.noreply.github.com>
2026-07-17 14:42:31 -0400

    archival and updates to letters

--- README.md
+++ README.md
@@ -3,6 +3,22 @@
 
 Notes on running a club for Hack Club leaders and school administrators - powered by [Next.js], [Theme UI] & [MDX].
 
+## Writing your own letters
+
+Wanna write a letter to school admins? First, find the category related to the letter you wanna write. If you want to make a category, scroll down to the Creating a category section :p
+
+Then, make a .mdx file in that folder, with the name of your letter, like `lynns-letter.mdx`. In that letter, you should start by putting the following:
+```
+---
+title: Orpheus's Email      # This is where you should put the title of your letter
+author: Orpheus     # This is where you should put your name!
+authorLink: https://hackclub.com        # This is where you should put your website. Optional, but always recommended.
+description: I am orpheus!      # A brief description of your article. Don't spend too much time on this lol
+---
+```
+
+When you're done writing, make a PR. It should be approved quickly, but if not, please poke in #leaders.
+
 ## Development
 
 Download the code to your computer:
--- pages/index.js
+++ pages/index.js
@@ -95,6 +95,102 @@ function Item({ name, label, description, icon, url }) {
   )
 }
 
+function SectionTitle({ title, icon, iconColor }) {
+  return (
+    <Flex sx={{ alignItems: 'center', justifyContent: ['center', 'flex-start'] }}>
+      {icon && (
+        <Flex
+          sx={{
+            bg: iconColor,
+            color: 'white',
+            p: 1,
+            mr: 2,
+            borderRadius: 6,
+            flexShrink: 0
+          }}
+        >
+          <Icon glyph={icon} size={24} />
+        </Flex>
+      )}
+      <Heading as="h1">{title}</Heading>
+    </Flex>
+  )
+}
+
+function Section({ folder }) {
+  const Markdown = require(`../content/${folder.description}`).default
+  const body = (
+    <>
+      <Markdown />
+      <Grid columns={[1, 2]}>
+        {folder.content.map(file => (
+          <Item
+            name={file.title}
+            description={file.description}
+            icon="docs"
+            key={file.name}
+            url={`view/${folder.name}/${file.name}`}
+          />
+        ))}
+      </Grid>
+    </>
+  )
+
+  if (folder.collapsable) {
+    return (
+      <Box
+        as="details"
+        sx={{
+          pt: 3,
+          mt: 3,
+          '&[open] summary .caret': { transform: 'rotate(90deg)' }
+        }}
+      >
+        <Flex
+          as="summary"
+          sx={{
+            cursor: 'pointer',
+            alignItems: 'center',
+            listStyle: 'none',
+            justifyContent: ['center', 'flex-start'],
+            '::-webkit-details-marker': { display: 'none' }
+          }}
+        >
+          <Box
+            className="caret"
+            sx={{
+              mr: 2,
+              width: 0,
+              height: 0,
+              borderTop: '6px solid transparent',
+              borderBottom: '6px solid transparent',
+              borderLeft: '8px solid currentColor',
+              transition: 'transform .125s ease-in-out'
+            }}
+          />
+          <SectionTitle
+            title={folder.title}
+            icon={folder.icon}
+            iconColor={folder.iconColor}
+          />
+        </Flex>
+        {body}
+      </Box>
+    )
+  }
+
+  return (
+    <Box sx={{ pt: 3, mt: 3 }}>
+      <SectionTitle
+        title={folder.title}
+        icon={folder.icon}
+        iconColor={folder.iconColor}
+      />
+      {body}
+    </Box>
+  )
+}
+
 function Content({ currentData, setCurrentData }) {
   useEffect(() => {
     currentData != null &&
@@ -284,33 +380,9 @@ export default function Home({ individualFiles, sections, generalBG }) {
             />
           ))}
         </Grid>
-        {sections.map((folder, index) => {
-          const Markdown = require(`../content/${folder.description}`).default
-          return (
-            <>
-              <Heading
-                pt={3}
-                mt={3}
-                as="h1"
-                sx={{ textAlign: ['center', 'left'] }}
-              >
-                {folder.title}
-              </Heading>
-              <Markdown />
-              <Grid columns={[1, 2]}>
-                {folder.content.map((file, index) => (
-                  <Item
-                    name={file.title}
-                    description={file.description}
-                    icon="docs"
-                    key={file.name}
-                    url={`view/${folder.name}/${file.name}`}
-                  />
-                ))}
-              </Grid>
-            </>
-          )
-        })}
+        {sections.map(folder => (
+          <Section folder={folder} key={folder.name} />
+        ))}
       </Container>
       <Box
         sx={{
